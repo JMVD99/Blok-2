f0afd939f5997074f9414d6573b8f0c78701334d
JMVD99 <56115546+JMVD99@users.noreply.github.com>
2019-11-18 08:35:39 +0100

    PHP Blok 2

---
+++ home.php
@@ -0,0 +1,10 @@
+<div class="row">
+    <div class="col-6">
+        <img class="images-fluid" scr="./IMG/Kado.jpg">
+    </div>
+    <div class="col-6">
+        <h1>    
+            <p>Verlanglijstje.org</p>
+            <p>De beste plaats voor Verlanglijstjes</p>
+            <p>Sinds 2019</p>
+        </h1>
---
+++ verlanglijstje.php
@@ -0,0 +1,16 @@
+<?php
+$leeftijd = 17;
+$voornaam = "Marco";
+$achternaam = "van Doeland";
+$naam = $voornaam . $achternaam;
+echo "<p>" . $leeftijd . "</p>";
+echo "<p>" . $voornaam . "</p>";
+echo "<p>" . $achternaam . "</p>";
+
+$getal1 = 3;
+$getal2 = 5;
+$som = $getal1 + $getal2;
+echo "<p>De waarde van de variabele is: " . $getal1 . "</p>";
+echo "<p>De waarde van de tweede variabele is: " . $getal2 . "</p>";
+echo "<p>De som is: " . $som . "</p>"
+?>
