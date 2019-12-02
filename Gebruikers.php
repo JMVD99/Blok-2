6307d15a00594af8d0a38f583c13e008d2ec5a97
JMVD99 <56115546+JMVD99@users.noreply.github.com>
2019-12-02 10:04:56 +0100

    Versie 1.1.5

--- Gebruikers.php
+++ Gebruikers.php
@@ -1,16 +1,21 @@
-<form>
-  <div class="form-group">
-    <label for="exampleInputEmail1">Email address</label>
-    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
-    <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
-  </div>
-  <div class="form-group">
-    <label for="exampleInputPassword1">Password</label>
-    <input type="password" class="form-control" id="exampleInputPassword1">
-  </div>
-  <div class="form-group form-check">
-    <input type="checkbox" class="form-check-input" id="exampleCheck1">
-    <label class="form-check-label" for="exampleCheck1">Check me out</label>
-  </div>
-  <button type="submit" class="btn btn-primary">Submit</button>
-</form>
+<div class="col-6">
+    <form>
+        <div class="form-group">
+            <label for="Firstname">Voornaam</label>
+            <input type="text" class="form-control" id="firstname" aria-describedby="firstnameHelp" name="firstname">
+            <small id="emailHelp" class="form-text text-muted">Vul hier uw voornaam in</small>
+        </div>
+        <div class="form-group">
+            <label for="Tussenvoegsel">Tussenvoegsel</label>
+            <input type="text" class="form-control" id="infix" aria-describedby="infixHelp" name="infix">
+            <small id="emailHelp" class="form-text text-muted">Als U geen tussenvoegsel heeft hoeft u niets in te
+                vullen.</small>
+        </div>
+        <div class="form-group">
+            <label for="Lastname">Achternaam</label>
+            <input type="text" class="form-control" id="lastname" aria-describedby="lastnameHelp" name="lastname">
+            <small id="emailHelp" class="form-text text-muted">Vul hier uw achternaam in</small>
+        </div>
+        <button type="submit" class="btn btn-primary">Submit</button>
+    </form>
+</div>
--- home.php
+++ home.php
@@ -1,10 +1,11 @@
 <div class="row">
-    <div class="col-6">
-        <img src="./IMG/Kado.jpg">
-    </div>
-    <div class="col-6">
-        <h1>    
-            <p>Verlanglijstje.org</p>
-            <p>De beste plaats voor Verlanglijstjes</p>
-            <p>Sinds 2019</p>
-        </h1>
+<div class="col-6" id="Home">
+    <img src="./IMG/Kado.jpg">
+</div>
+<div class="col-6">
+    <h1>
+        <p>Verlanglijstje.org</p>
+        <p>De beste plaats voor Verlanglijstjes</p>
+        <p>Sinds 2019</p>
+    </h1>
+</div>
